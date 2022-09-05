962231ba7d787d4a29435f17eecf47d60992831c
marurevi <maryrev@hotmail.com>
2022-09-05 18:30:07 -0300

    Add group validations and test

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :group do
-    name { "Group_name" }
-    icon { "icon" }
-    user 
+    name { 'Group_name' }
+    icon { 'icon' }
+    user
   end
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @group = FactoryBot.create(:group, user: @user)
+  end
+  describe 'validations' do
+    describe 'A group has a name' do
+      it 'should validate the presence of a name' do
+        expect(@group).to be_valid
+      end
+
+      it 'should not be valid without a name' do
+        @group.name = nil
+        expect(@group).to_not be_valid
+      end
+    end
+  end
 end
