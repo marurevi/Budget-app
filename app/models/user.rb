7d42540eaf6d95dba2f4a91ea8ea6105a900d53e
marurevi <maryrev@hotmail.com>
2022-09-05 17:49:38 -0300

    Add user validations and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  
+
   has_many :operations, dependent: :destroy
   has_many :groups, dependent: :destroy
 
   validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
