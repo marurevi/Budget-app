b20f19f6dcce015cc8208cdcf8d96a4ded913ecc
marurevi <maryrev@hotmail.com>
2022-09-05 15:42:17 -0300

    Create group model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   
   has_many :operations, dependent: :destroy
-  
+  has_many :groups, dependent: :destroy
+
   validates :name, presence: true
 end
