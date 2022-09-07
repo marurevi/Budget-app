cf043c0e9f9532c1fa2d494811d37e26a4064c7d
marurevi <maryrev@hotmail.com>
2022-09-07 19:21:23 -0300

    Create seed

--- db/schema.rb
+++ db/schema.rb
@@ -14,15 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_190351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "group_operations", force: :cascade do |t|
-    t.bigint "group_id", null: false
-    t.bigint "operation_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_group_operations_on_group_id"
-    t.index ["operation_id"], name: "index_group_operations_on_operation_id"
-  end
-
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "icon"
@@ -30,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_190351) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "groups_operations", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "operation_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_groups_operations_on_group_id"
+    t.index ["operation_id"], name: "index_groups_operations_on_operation_id"
   end
 
   create_table "operations", force: :cascade do |t|
@@ -58,8 +58,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_190351) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "group_operations", "groups"
-  add_foreign_key "group_operations", "operations"
   add_foreign_key "groups", "users"
+  add_foreign_key "groups_operations", "groups"
+  add_foreign_key "groups_operations", "operations"
   add_foreign_key "operations", "users"
 end
