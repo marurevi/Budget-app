d364b0ff5a2d02a870355764533e4b3dd8215c08
marurevi <maryrev@hotmail.com>
2022-09-07 15:29:31 -0300

    Write test for new view in group controller

---
+++ spec/system/groups/new_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'groups#new', type: :system do
+  let(:user) { FactoryBot.create(:user) }
+  let(:group) { FactoryBot.create(:group, user:) }
+
+  before do
+    user.confirm
+    sign_in(user)
+    visit new_group_path
+  end
+
+  context 'when user is visit the page' do
+    it 'shows the form to create a new group' do
+      expect(page).to have_content 'Add new category:'
+      expect(page).to have_content 'Name'
+      expect(page).to have_content 'Icon'
+      expect(page).to have_content 'Add new'
+      expect(page).to have_content 'Back'
+    end
+
+    context 'when the user fill the form with valid data' do
+      it 'creates a new group' do
+        fill_in 'Name', with: 'Group_name'
+        fill_in 'Icon', with: 'icon'
+        click_button 'Add new'
+        visit groups_path
+        expect(page).to have_content 'Group_name'
+      end
+    end
+  end
+end
