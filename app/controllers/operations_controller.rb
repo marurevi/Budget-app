e7838970fc0bfda04e385afa9fdf887b04a93cca
marurevi <maryrev@hotmail.com>
2022-09-07 19:26:14 -0300

    Implement functionality to operations' index view and controller acording to the test

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,12 +1,34 @@
 class OperationsController < ApplicationController
   before_action :authenticate_user!
 
-  def new; end
-  def index; end
+  def new
+    @group = Group.find(params[:group_id])
+    @operation = Operation.new
+  end
+
+  def index
+    @group = Group.find(params[:group_id])
+    @operations = @group.operations.order('created_at DESC')
+    @total = @operations.sum(:amount)
+  end
+
+  def create
+    @operation = current_user.operations.build(operation_params)
+
+    if @operation.save
+      redirect_to group_operations_path(@group)
+    else
+      render :new
+    end
+  end
 
   private
 
   def authenticate_user!
     redirect_to root_path unless user_signed_in?
+  end
+
+  def operation_params
+    params.require(:operation).permit(:name, :amount)
   end
 end
