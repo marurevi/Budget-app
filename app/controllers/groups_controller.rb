88c9c36b9a5a47e127c81feb5efe6c828f7a6e33
marurevi <maryrev@hotmail.com>
2022-09-07 13:54:04 -0300

    Implement changes in group controller and index view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,30 @@
 class GroupsController < ActionController::Base
-  def index; end
+  before_action :authenticate_user!
+
+  def new
+    @group = Group.new
+  end
+
+  def index
+    @categories = Group.where(user: current_user)
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
+
+  def authenticate_user!
+    redirect_to root_path unless user_signed_in?
+  end
 end
