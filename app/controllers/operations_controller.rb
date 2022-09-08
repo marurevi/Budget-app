61e85d9a275bb7b6849dfc802cbed777a95505ed
marurevi <maryrev@hotmail.com>
2022-09-08 16:58:39 -0300

    Implement changes on operation controllers acording to test

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -13,9 +13,11 @@ class OperationsController < ApplicationController
   end
 
   def create
+    @group = Group.find(params[:group_id])
     @operation = current_user.operations.build(operation_params)
 
     if @operation.save
+      @operation.groups << groups(@group)
       redirect_to group_operations_path(@group)
     else
       render :new
@@ -30,5 +32,12 @@ class OperationsController < ApplicationController
 
   def operation_params
     params.require(:operation).permit(:name, :amount)
+  end
+
+  def groups(default_group)
+    groups = [default_group]
+
+    groups << Group.where(id: params[:operation][:group_ids])
+    groups.flatten
   end
 end
